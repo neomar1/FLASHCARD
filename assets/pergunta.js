dd17810ddd9693f4e22e8471c5f0636866a254a0
neomar1 <159184966+neomar1@users.noreply.github.com>
2024-10-01 16:39:49 -0300

    neoma gay

---
+++ assets/pergunta.js
@@ -0,0 +1,15 @@
+criaCartao(
+     'progamação',
+     'O que Python',
+     'o pyhon é uma linguagem de propagação'
+)
+criaCartao(
+'Geografia',
+'Qual é a capital da França',
+'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
